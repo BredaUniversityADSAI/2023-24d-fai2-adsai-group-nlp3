9eeb37f390eab14b519c6c01fe308f52c8f13ac1
jurajkret221439 <221439@buas.nl>
2024-05-11 20:30:05 +0200

    test

--- Streamli-API/app/streamlit-app.py
+++ Streamli-API/app/streamlit-app.py
@@ -1,1 +1,12 @@
-gun 
+import streamlit as st
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+st.set_page_config(
+    page_title="Episode Data Preprocessing",
+    page_icon="🧊",
+)
+
+st.title("Episode Data Preprocessing")
+st.
