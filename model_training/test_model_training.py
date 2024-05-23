a6374a6ad659138fceba9ad25dd383b4d23c4d4f
MaxMeiners214936 <112880008+MaxMeiners@users.noreply.github.com>
2024-05-23 21:30:58 +0200

    Update test_model_training.py

--- model_training/test_model_training.py
+++ model_training/test_model_training.py
@@ -1,19 +1,17 @@
 import unittest
 import model_training
-from transformers import RobertaTokenizer
+from transformers import RobertaTokenizer, TFRobertaForSequenceClassification
 from sklearn.preprocessing import LabelEncoder
 import tensorflow as tf
+import pandas as pd
 
 class TestModelTraining(unittest.TestCase):
 
     def test_load_data(self):
         file_path = '/Users/maxmeiners/Documents/GitHub/2023-24c-fai2-adsai-MaxMeiners/Datasets_new/emotions_all_V6.csv'
         dataset = 'emotions_all_V6.csv'
-        nrows = 10000
-        df = model_training.load_data(file_path, dataset, nrows)
-        self.assertIsNotNone(df)
-        self.assertEqual(len(df), 10000)
-        
+        self.assertIsNotNone(model_training.load_data(file_path, dataset))
+
     def test_get_model(self):
         model_path = '/Users/maxmeiners/Downloads/model'
         num_classes = 6
@@ -26,11 +24,27 @@ class TestModelTraining(unittest.TestCase):
 
     def test_predict(self):
         model_path = '/Users/maxmeiners/Downloads/model'
-        model = tf.keras.models.load_model(model_path)
+        model = TFRobertaForSequenceClassification.from_pretrained(model_path)
+
         sentences = '/Users/maxmeiners/Documents/GitHub/2023-24c-fai2-adsai-MaxMeiners/Datasets_new/emotions_all.csv'
         tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+        
+        # Load the data and fit the LabelEncoder
+        df = pd.read_csv(sentences)
         label_encoder = LabelEncoder()
+        label_encoder.fit(df['emotion'].values)
         self.assertIsNotNone(model_training.predict(model, sentences, tokenizer, label_encoder))
+
+    def test_evaluate(self):
+        eval_data = '/Users/maxmeiners/Documents/GitHub/2023-24c-fai2-adsai-MaxMeiners/Datasets_new/emotions_all.csv'
+
+        model_path = '/Users/maxmeiners/Downloads/model'
+        model = TFRobertaForSequenceClassification.from_pretrained(model_path)
+
+        tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+        
+        label_encoder = LabelEncoder()
+        self.assertIsNotNone(model_training.evaluate(eval_data, model, tokenizer, label_encoder))
 
 if __name__ == '__main__':
     unittest.main()
