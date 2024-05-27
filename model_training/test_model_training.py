65a79947d986c54489871b3b1fe04fb68f5ee3ca
MaxMeiners214936 <112880008+MaxMeiners@users.noreply.github.com>
2024-05-27 12:58:45 +0200

    Update test_model_training.py

--- model_training/test_model_training.py
+++ model_training/test_model_training.py
@@ -5,23 +5,29 @@ from sklearn.preprocessing import LabelEncoder
 import tensorflow as tf
 import pandas as pd
 
-class TestModelTraining(unittest.TestCase):
+class TestModelTraining:
 
     def test_load_data(self):
         file_path = '/Users/maxmeiners/Downloads/model/test_emotions'
         dataset = 'emotions_all_V6.csv'
-        self.assertIsNotNone(model_training.load_data(file_path, dataset))
+        self.assertIsNotNone(model_training.load_data(
+            file_path, 
+            dataset))
 
     def test_get_model(self):
         model_path = '/Users/maxmeiners/Downloads/model'
         num_classes = 6
-        self.assertIsNotNone(model_training.get_model(model_path, num_classes))
+        self.assertIsNotNone(model_training.get_model(
+            model_path, 
+            num_classes))
 
     def test_preprocess_data(self):
         file_path = '/Users/maxmeiners/Downloads/model/test_emotions'
         df = pd.read_csv(file_path)
         tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
-        self.assertIsNotNone(model_training.preprocess_data(df, tokenizer))
+        self.assertIsNotNone(model_training.preprocess_data(
+            df, 
+            tokenizer))
 
     def test_predict(self):
         model_path = '/Users/maxmeiners/Downloads/model'
@@ -34,7 +40,11 @@ class TestModelTraining(unittest.TestCase):
         df = pd.read_csv(sentences)
         label_encoder = LabelEncoder()
         label_encoder.fit(df['emotion'].values)
-        self.assertIsNotNone(model_training.predict(model, sentences, tokenizer, label_encoder))
+        self.assertIsNotNone(model_training.predict(
+            model, 
+            sentences, 
+            tokenizer, 
+            label_encoder))
 
     def test_evaluate(self):
         eval_data_path = '/Users/maxmeiners/Downloads/model/test_emotions_eval'
@@ -46,7 +56,11 @@ class TestModelTraining(unittest.TestCase):
         tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
         label_encoder = LabelEncoder()
         label_encoder.fit(eval_data['emotion'].values)
-        self.assertIsNotNone(model_training.evaluate(eval_data, model, tokenizer, label_encoder))
+        self.assertIsNotNone(model_training.evaluate(
+            eval_data, 
+            model, 
+            tokenizer, 
+            label_encoder))
 
 if __name__ == '__main__':
     pytest.main()
